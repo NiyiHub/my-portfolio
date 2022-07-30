0820a185d76cc740344b456f4ef1d15834fc627e
NiyiHub <niyiblack@outlook.com>
2022-07-30 15:22:19 +0100

    Added bootstrap and projects are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Blog(models.Model):
+	title						= models.CharField(max_length=255)
+	pub_date					= models.DateTimeField(auto_now_add=True)
+	body						= models.TextField(max_length=500)
+	image						= models.ImageField(upload_to='Images/')
