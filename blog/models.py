16efcbfc1c0938cd164b14318f64508f07d6b18d
NiyiHub <niyiblack@outlook.com>
2022-08-03 02:48:03 +0100

    Final Polish

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
 	pub_date					= models.DateTimeField(auto_now_add=True)
 	body						= models.TextField(max_length=500)
 	image						= models.ImageField(upload_to='Images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_cut(self):
+		return self.pub_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def blogs(request):
+	blogss = Blog.objects
+	return render(request, 'blog/blogs.html', {'blogss':blogss})
+
+def detail(request, blog_id):
+	blog_detail = get_object_or_404(Blog, pk=blog_id)
+	return render(request, 'blog/detail.html', {'blog':blog_detail})
