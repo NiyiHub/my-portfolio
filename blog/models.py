bc38f46f23550f0ce30316d9a0b32b9bb71e7a3a
NiyiHub <niyiblack@outlook.com>
2022-09-23 01:47:49 +0100

    New DBMS set up.

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db.models import Model
 class Blog(models.Model):
 	title						= models.CharField(max_length=255)
 	pub_date					= models.DateTimeField(auto_now_add=True)
-	body						= models.TextField(max_length=500)
+	body						= models.TextField()
 	image						= models.ImageField(upload_to='Images/')
 
 	def __str__(self):
--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 class Project(models.Model):
+	title					= models.CharField(max_length=200)
 	image					= models.ImageField(upload_to='images/')
-	description				= models.CharField(max_length=200)
+	description				= models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.description[:100]
+
+
